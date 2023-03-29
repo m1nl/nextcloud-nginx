2f3c3cc1424cd96765da46bd21db372ba7fab5c5
Mateusz Nalewajski <mateusz@nalewajski.pl>
2023-03-29 16:12:39 +0200

    update nginx configuration, bump version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,7 @@ NGINX="/usr/sbin/nginx"
 NGINX_CONF="/etc/nginx/nginx.conf"
 NGINX_CONF_TEMPLATE="/etc/nginx/nginx.conf.template"
 
-export MAX_BODY_SIZE="${MAX_BODY_SIZE:-512M}"
+export MAX_BODY_SIZE="${MAX_BODY_SIZE:-10240M}"
 export PHP_FPM_URL="${PHP_FPM_URL:-nextcloud-fpm:9000}"
 
 [ -f $NGINX_CONF_TEMPLATE ] && $GOMPLATE -f "$NGINX_CONF_TEMPLATE" -o "$NGINX_CONF"
